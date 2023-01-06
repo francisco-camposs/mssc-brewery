163cc67b281991ed3657e11b110e244b9927e228
Francisco <campos.junior.ifrn@gmail.com>
2023-01-05 21:30:47 -0300

    completing customer assignment

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerDTO getCustomerById(UUID customerId) {
+        return CustomerDTO.builder()
+                .id(UUID.randomUUID())
+                .name("John Doe")
+                .build();
+    }
+
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDTO.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+
+    private UUID id;
+    private String name;
+
+}
